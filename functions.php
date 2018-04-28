a53b65f334e8655926c243697a3f247562a1b208
scs <stephensmithdesigns@gmail.com>
2018-04-28 12:37:34 +0200

    trying to change to color purple

--- functions.php
+++ functions.php
@@ -1,18 +1,12 @@
 <?php
-require_once( 'wp-less/wp-less.php' );
-if ( ! is_admin() )
-    wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/less/style.less' );
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 function my_theme_enqueue_styles() {
+
 
     $parent_style = 'richone-mainstyle';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
