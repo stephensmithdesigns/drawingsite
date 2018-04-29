7c6799e880b247cf8f723300d3a07d481255790e
scs <stephensmithdesigns@gmail.com>
2018-04-29 21:57:19 +0200

    changing styles

--- functions.php
+++ functions.php
@@ -4,9 +4,6 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 function my_theme_enqueue_styles() {
 
-
-    $parent_style = 'richone-mainstyle';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri() );
 }
 ?>
